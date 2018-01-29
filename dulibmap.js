339deb2dc56dd23714961b316c81357cc2aeefd5
Jeff <you@example.com>
2018-01-28 18:53:30 -0700

    Add menu

--- dulibmap.js
+++ dulibmap.js
@@ -2,7 +2,7 @@
 
 var init = function() {
 	// Add map container
-	document.getElementById("dulibmap").innerHTML = "<div id='map-container'></div>";
+	var main = document.getElementById("dulibmap").innerHTML = "<div id='map-container'></div><div id='select-menu'></div>";
 	var container = document.getElementById("map-container");
 
 	// Add upper level
@@ -12,6 +12,7 @@ var init = function() {
 	container.appendChild(top_floor);
 
 	addBaseImages();
+	addSlideSelectMenu();
 }
 
 var addBaseImages = function() {
@@ -20,17 +21,32 @@ var addBaseImages = function() {
 
 	top_base.setAttribute("id", "top_base");
 	top_base.setAttribute("class", "map-base");
-	top_base.setAttribute("src", "./assets/img/map/top_base.png");
+	top_base.setAttribute("src", "./assets/img/map/base/top_base.png");
 	top_floor.appendChild(top_base);
 
 	var testImg = document.createElement("IMG");
 	testImg.setAttribute("id", "test-img");
 	testImg.setAttribute("class", "map-slide");
-	testImg.setAttribute("src", "./assets/img/map/top_frr.gif");
+	testImg.setAttribute("src", "./assets/img/map/slide/top_frr.gif");
 	top_floor.appendChild(testImg);
 }
 
-var onSelectRoom = function() {
+var addSlideSelectMenu = function() {
+	var menu = document.getElementById("select-menu");
+		select_form = document.createElement("FORM");
+
+	var slide;
+	for(var key in config.slides) {
+		slide = document.createElement("INPUT");
+		slide.setAttribute("type", "checkbox");
+		slide.setAttribute("name", key);
+		slide.setAttribute("value", key);
+		select_form.appendChild(slide);
+	}
+	menu.appendChild(select_form);
+}
+
+var onSelectRoom = function(test) {
 
 }
 
