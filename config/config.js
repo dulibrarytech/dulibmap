4a373f32a7354f6c6a8b4167b92ee4ed11830da6
Jeff <you@example.com>
2018-08-06 19:44:42 -0600

    Top floor base image update

--- config/config.js
+++ config/config.js
@@ -9,22 +9,34 @@ var config = {
 
 		"top_floor" : {
 
-			"Study Rooms" : {
+			"Reading Rooms" : {
 
 				"top_frr"			: "Faculty Reading Room",
 				"top_euc"			: "Extended Use Carrels",
 				"top_gr"			: "Gottesfeld Room",
-				"top_qrr"			: "Quinette Reading Room"
+				"top_qrr"			: "Quinette Reading Room",
+				"top_dfra"			: "Deline Family Reading Area"
+
 			},
 
 			"Offices" : {
 
-				"top_cfwlc"			: "Center for World Languages and Cultures"
+				"top_ds"			: "Dean's Suite",
+				"top_cfwlc"			: "Center for World Languages and Cultures",
+				"top_wpf"			: "Writing Program Faculty",
+				"top_otl" 			: "Office of Teaching and Learning"
+			},
+
+			"Study Rooms" : {
+
+				"top_sgs"			: "Small Group Study",
+				"top_lgs"			: "Large Group Study"
 			},
 
 			"Meeting Rooms" : {
 
-				"top_loft"			: "The Loft"
+				"top_loft"			: "The Loft",
+				"top_wtr"			: "Wohlauer Teleconference Room"
 			}
 		},
 
@@ -44,7 +56,12 @@ var config = {
 		"top_euc"   : "#e0f992",
 		"top_gr"	: "#e8c9fb",
 		"top_qrr"	: "#f3c5c5",
-		"top_loft"	: "#fdde89"
+		"top_loft"	: "#fdde89",
+		"top_dfra"	: "#eed8fb",
+		"top_sgs" 	: "#cecfae",
+		"top_sgs" 	: "#afcc93",
+		"top_wtr"	: "#e0f992",
+		"top_otl" 	: "#eed8fb"
 	},
 
 	"labels" : {
